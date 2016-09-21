56bdfacaef1b4db455fd7ce323c859f2a1e8794a
HungHN <hung.hoang@ntq-solution.com.vn>
2016-09-21 17:40:14 +0700

    Initial Commit

---
+++ iOsProject1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOsProject1
+//
+//  Created by ntq on 9/21/16.
+//  Copyright © 2016 HungHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
